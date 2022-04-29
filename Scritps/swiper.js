09f3f6d6c0958ebe0238eeca998f018df3f7b200
Szakács Gergő <szkcsgrg@gmail.com>
2022-04-29 19:37:58 +0200

    Updated Projects

--- Scritps/swiper.js
+++ Scritps/swiper.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     autoplay: {
-     delay: 10000,
+     delay: 15000,
    },
     // Navigation arrows
     navigation: {
